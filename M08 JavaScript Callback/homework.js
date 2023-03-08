ab2ab01b3079acb6c82f03b2428a36558dc339b9
hernandiaz94 <hernan.diiaz@gmail.com>
2023-03-08 19:55:48 -0300

    homework m08 done

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -57,10 +57,19 @@ function filter(arrayOfStrings) {
   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
   // Luego retorna un nuevo arreglo con estos elementos.
   // Tu código:
-  const filteredArray = arrayOfStrings.filter((element) =>
-    element.empiezaCon("a")
+  let newArray=[];
+  
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    if (arrayOfStrings[i].charAt(0) === "a") {
+      newArray.push(arrayOfStrings[i]);
+    }
+  }
+  return newArray;
+
+  /* const filteredArray = arrayOfStrings.filter((element) =>
+    element.startsWith("a")
   );
-  return filteredArray;
+  return filteredArray; */
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
